d547fe1cf9037e39a520ba148d32de0d1c8e31f5
wizzud <wizzud@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-05 23:32:25 +0000

    1.1.0 updates
    git-svn-id: https://plugins.svn.wordpress.org/custom-menu-wizard/trunk@751958 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-menu-wizard.js
+++ custom-menu-wizard.js
@@ -1,28 +1,52 @@
 /* Plugin Name: Custom Menu Wizard
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Roger Barrett
  * 
  * Script for controlling this widget's options (in Admin -> Widgets)
 */
 jQuery(function($){
-	var dotPrefix = '.widget-custom-menu-wizard'
-	$(document).on('click', dotPrefix + '-collapsible-fieldset', function(){
-		var chkbox = $('input', this).eq(0),
-				collapse = !chkbox.prop('checked');
-		if(chkbox.length){
-			chkbox.prop('checked', collapse);
-			$('div', this).css({backgroundPosition:collapse?'0 0':'0 -36px'});
-			$(this).next('div')[collapse?'slideUp':'slideDown']();
-		}
-		this.blur();
-		return false;
-	});
-	$(document).on('change', dotPrefix + '-filter-radio', function(){
-		$(dotPrefix + '-filter-radio-dep', this.form).prop('disabled', $(dotPrefix + '-filter-radio', this.form).eq(0).prop('checked'));
-	});
-	$(document).on('change', dotPrefix + '-select-menu', function(){
-		var optgroups = $('select' + dotPrefix + '-filter-radio-dep', this.form).val(0).find('optgroup');
-		optgroups.filter(':visible').hide();
-		optgroups.eq( this.selectedIndex ).show();
-	});
+	var dotPrefix = '.widget-custom-menu-wizard',
+			filterItems = $('select' + dotPrefix + '-listen');
+	$(document)
+		//fieldsets...
+		.on('click', dotPrefix + '-collapsible-fieldset', function(){
+			var chkbox = $('input', this).eq(0),
+					collapse = !chkbox.prop('checked');
+			if(chkbox.length){
+				chkbox.prop('checked', collapse);
+				$('div', this).css({backgroundPosition:collapse?'0 0':'0 -36px'});
+				$(this).next('div')[collapse?'slideUp':'slideDown']();
+			}
+			this.blur();
+			return false;
+		})
+		//change of menu...
+		.on('change', dotPrefix + '-selectmenu', function(){
+			var select = $('select' + dotPrefix + '-listen', this.form),
+					from = $('#' + select.attr('id') + '_ignore').find('optgroup'),
+					groupClone;
+			if(from.length > this.selectedIndex){
+				if(select.val() > 0){
+					select.val(0);
+				}
+				groupClone = from.eq( this.selectedIndex ).clone();
+				groupClone.find('option[selected]').removeAttr('selected').prop('selected', false);
+				select.find('optgroup').remove();
+				select.append(groupClone).trigger('change');
+			}
+		})
+		//enableif and disableif...
+		.on('change', dotPrefix + '-listen', function(){
+			var listeners = $(dotPrefix + '-listen', this.form),
+					showAll = listeners.eq(0).prop('checked'),
+					rootParent = !showAll && listeners.filter('select').val() < 0;
+			$(dotPrefix + '-disableif', this.form).css({color:showAll ? '#999999' : 'inherit'}).find('input,select').prop('disabled', showAll);
+			$(dotPrefix + '-enableif', this.form).css({color:!rootParent ? '#999999' : 'inherit'}).find('input,select').prop('disabled', !rootParent);
+		});
+	//remove non-active optgroups...
+	filterItems.find('optgroup').filter(function(){
+		return !$(this).data('cmwActiveMenu');
+	}).remove();
+	//trigger change...
+	filterItems.trigger('change');
 });
